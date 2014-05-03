054e9abbec2712e3e1d5eab6225b9f3bea6e1bd4
hippo75100 <hippo75100@hippo75100tekiMacBook-Air.local>
2014-05-03 20:21:18 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+console.log("hi!");
+var http = require("http");
+var request = require("request");
+var port = 1337;
+var url = "http://graph.facebook.com/2014METEORmeethere/photos?type=uploaded";
+var fs = require('fs');
+
+fs.readfile("./index.html","utf8", function (err,result){
+	template = result;
+})
+
+template = fs.readFileSync("./index.html","utf8");
+
+http.createServer(function (req, res)
+{
+    var data= "";
+    request.get(url, function (err,body,result)
+    {
+      result = JSON.parse(result);
+      result.data.forEach(function(val,idx){
+      	data += val.images[2].source;})     
+
+      data = template.replace("{{content}}",data);
+
+          res.end(data);
+});
+
+
+})
+.listen(port);
+
+console.log("sever create:" + port);
+
